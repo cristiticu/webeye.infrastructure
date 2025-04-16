720e29f2729df33802e3dabacb359a57880ab526
Cristian Ticu <cristian.ticu@eggnita.com>
2025-04-16 21:08:17 +0300

    Update version

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -50,7 +50,7 @@ for (const region of regions) {
         environment,
         regionName: region,
         repositoryName: 'webeye.checker.ecr',
-        imageTag: 'webeye.speed-checker_latest15Apr2025-7',
+        imageTag: 'webeye.speed-checker_latest16Apr2025-3',
     });
 }
 
